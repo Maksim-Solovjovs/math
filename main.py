14b4976a06cd656c00d5c4c1b638dfd5476ea3d6
Max <77140683+Maksim-Solovjovs@users.noreply.github.com>
2022-04-19 09:47:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+number=8.50
+print("округлить до большего 8.50 это:",math.ceil(number))
+print("округлить до меньшего 8.50 это:",math.floor(number))  
+#import math    
+#numbers=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,8.9]    
+#print("сумма: ", numbers, ":", math.fsum(numbers))            
+#print("сумма чисел от 1 до 10:", math.fsum(range(1,11)))    
+#import math  
+#print(math.copysign(-4, 1))
+#print(math.copysign(-8, 97.21))
+#print(math.copysign(-43, -76))
+#import math  
+#print(math.isclose(2, 2))
+#print(math.isclose(3, 3))
+#print(math.isclose(34, 4))
+#print(math.isclose(33, 1))
+#import math
+#print("math.modf(100.12) : ", math.modf(100.12))
+#print("math.modf(-100.72) : ", math.modf(-100.72)) 
+#print("math.modf(2) : ", math.modf(2)) 
